7f27370b941cb45f01bae7cd680bcae897296490
azmprllynsa <azmi.naisa@gmail.com>
2020-04-26 09:17:33 +0700

    add cart table

---
+++ migrations/20200425211009-create-cart.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.STRING
+      },
+      product_id: {
+        type: Sequelize.STRING
+      },
+      quantity: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('carts');
+  }
+};
